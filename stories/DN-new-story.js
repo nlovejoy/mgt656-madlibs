4bb4abd9519e8d98cedff70ce4a8383770fff440
Nick <nicholas.lovejoy@yale.edu>
2015-09-30 09:20:32 -0400

    Added a new story

---
+++ stories/DN-new-story.js
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        Once upon a time, in a {{adjectives.get()}} village, there sat a {{adjectives.get()}} {{nouns.get()}}.  
+ From his remote village, this {{adjective.get()}} {{nouns.get()}} claimed that he was a better coder than this guy... <img src="http://i.ytimg.com/vi/15HOxJugBn0/hqdefault.jpg" />
+
+<br><b>A {{adjectives.get()}} battle ensues.</b>  During the battle, all the villagers {{verbs.get()}}, while some {{verbs.get()}} over the {{adjectives.get()}} {{nouns.get()}} in fear.  
+    </body>
+</html>
